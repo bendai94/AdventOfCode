c6386af32370807a33a9dcc10392a602593f973b
Ben Dailey <bendai94@gmail.com>
2022-12-05 10:23:15 -0500

    Completes Day 1

--- 2022/app.ts
+++ 2022/app.ts
@@ -1,2 +1,59 @@
-let message: string = "Hello world";
+import * as fs from 'fs';
+
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename): string[] {
+    const contents = readFileSync(filename, 'utf-8');
+
+    const arr = contents.split(/\r?\n/);
+
+    
+    return arr;
+}
+
+
+let list = syncReadFile('./day1.txt');
+
+// console.log(list);
+
+let One = 0;
+let Two = 0;
+let Thr = 0;
+
+let currentcount = 0;
+
+list.forEach(element => {
+    if (!element)
+    {
+        checkCount(currentcount);
+        currentcount = 0;
+    }
+    else
+    {
+        currentcount += +element;
+    }
+})
+
+function checkCount(count) {
+    if (currentcount > Thr)
+    {
+        Thr = currentcount;   
+    }
+    if (currentcount > Two)
+    {
+        Thr = Two;
+        Two = currentcount;
+    }
+    if (currentcount > One)
+    {
+        Thr = Two;
+        Two = One;
+        One = currentcount;
+    }
+}
+
+console.log(One + Two + Thr);
+
+
+let message: string = "End Of Line";
 console.log(message);
